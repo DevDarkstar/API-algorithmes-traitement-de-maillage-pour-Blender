72ceee291012eac8859ad44876f721ca76e09d19
DevDarkstar <richard.leestmans@live.fr>
2025-01-20 05:06:48 +0100

    Ajout algorithme de remaillage de nuages de points
    Implémente un algorithme composé de plusieurs sous-algorithmes permettant de transformer un nuage de points en surface maillée.

--- api_traitements_maillage/api_modules/py_mesh.py
+++ api_traitements_maillage/api_modules/py_mesh.py
@@ -19,18 +19,27 @@ class PyMeshApi:
             vertices = np.reshape(vertices, (vertices.shape[0] // 3, 3))
         else:
             raise RuntimeError("Le dictionnaire des données ne possède pas les coordonnées des sommets.")
-
-        # Pareil pour les indices des sommets des faces
-        faces = self.data.get("faces", None)
-        if faces is not None:
-            faces = np.reshape(faces, (faces.shape[0] // 3, 3))
-        else:
-            raise RuntimeError("Le dictionnaire des données ne possède pas les indices des sommets des faces.")
         
         # Récupération du nom de la fonction MeshLab utilisée pour exécuter l'algorithme
         functions_name = self.data.get("function", None)
         if functions_name is None:
             raise RuntimeError("Le dictionnaire des données ne possède pas le nom de la fonction MeshLab à exécuter.")
+
+        # Pareil pour les indices des sommets des faces
+        faces = self.data.get("faces", np.array([], dtype=np.int32).reshape(0,3))
+        if faces.size > 0:
+            faces = np.reshape(faces, (faces.shape[0] // 3, 3))
+        
+        # Récupération d'éventuelles informations de couleurs du maillage
+        # Pour les sommets
+        vertex_color = self.data.get("vertex_color", np.array([], dtype=np.float64).reshape(0,4))
+        if vertex_color.size > 0:
+            vertex_color = np.reshape(vertex_color, (vertex_color.shape[0] // 4, 4))
+        
+        # Pour les faces
+        face_color = self.data.get("face_color", np.array([], dtype=np.float64).reshape(0,4))
+        if face_color.size > 0:
+            face_color = np.reshape(face_color, (face_color.shape[0] // 4, 4))
         
         # Récupération des éventuels paramètres utilisés pour la fonction MeshLab
         params = self.data.get("params")
@@ -39,7 +48,7 @@ class PyMeshApi:
         ms = pymeshlab.MeshSet()
 
         # Création d'un maillage à partir des coordonnées des sommets et des indices des sommets des faces
-        mesh = pymeshlab.Mesh(vertex_matrix=vertices, face_matrix=faces)
+        mesh = pymeshlab.Mesh(vertex_matrix=vertices, face_matrix=faces, v_color_matrix=vertex_color, f_color_matrix=face_color)
 
         # Ajout du maillage dans le MeshSet
         ms.add_mesh(mesh)
@@ -59,6 +68,19 @@ class PyMeshApi:
             self.result["output_result"] = ["replace_mesh"]
             self.result["vertices"] = resulting_mesh.vertex_matrix().flatten()
             self.result["faces"] = resulting_mesh.face_matrix().flatten()
+        elif all(function_name in ["generate_simplified_point_cloud", "compute_normal_for_point_clouds", "generate_surface_reconstruction_ball_pivoting"] for function_name in functions_name):
+            self.result["output_result"] = ["replace_mesh"]
+            self.result["vertices"] = resulting_mesh.vertex_matrix().flatten()
+            self.result["faces"] = resulting_mesh.face_matrix().flatten()
+            # Récupération d'informations sur les couleurs si présentes
+            if resulting_mesh.has_vertex_color:
+                self.result["output_result"].append("vertex_coloration")
+                self.result["colors"] = resulting_mesh.vertex_color_matrix().flatten()
+            elif resulting_mesh.has_face_color:
+                self.result["output_result"].append("face_coloration")
+                self.result["colors"] = resulting_mesh.face_color_matrix().flatten()
+            else:
+                pass
         else:
             raise RuntimeError("La fonction MeshLab demandée n'est pas encore implantée dans l'extension.")
 
